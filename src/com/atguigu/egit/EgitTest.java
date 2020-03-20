16976a764ad858468318150cea7840ee83ac894f
qwer0016 <15614079696.136.com>
2020-03-21 00:54:25 +0800

    2222222222222222

--- src/com/atguigu/egit/EgitTest.java
+++ src/com/atguigu/egit/EgitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.egit;
 public class EgitTest {
 	public static void main(String[] args) {
 		System.out.println("111111111111111111");
+		System.out.println("2222222222222222");
 	}
 }
