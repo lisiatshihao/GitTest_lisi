814bdc38876e101a590a6249d6381018aeeb9316
wangwuatshihao <q2019_1@163.com>
2020-03-22 23:57:17 +0800

    创建了一个新分支王五创建

--- src/com/atguigu/egit/EgitTest.java
+++ src/com/atguigu/egit/EgitTest.java
@@ -7,6 +7,9 @@ public class EgitTest {
 		System.out.println("3333333333333333");
 		System.out.println("44444");
 		System.out.println("555555555555555555");
+		System.out.println("777777777777");
 		System.out.println("66666666");
+		System.out.println("创建了一个新分支王五创建");
+
 	}
 }
